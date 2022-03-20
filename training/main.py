42ce7b30db797a3f2a76f81de71861e9220702de
BoyuanJackChen <bc3194@nyu.edu>
2022-03-20 14:02:08 -0400

    Now saving checkpoints

--- training/main.py
+++ training/main.py
@@ -10,6 +10,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
+import os
 from torch.optim.lr_scheduler import StepLR
 
 from preprocessing import mixup_data, mixup_criterion
@@ -18,15 +19,15 @@ from wide_resnet import Wide_ResNet
 from utils import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--epochs', type=int, default=200)
+parser.add_argument('--epochs', type=int, default=300)
 parser.add_argument('--checkpoint', type=int, default=20)
-parser.add_argument('--lr', type=float, default=1e-5)
+parser.add_argument('--lr', type=float, default=1e-4)
 parser.add_argument('--train_batch', type=int, default=64, help="batch size for training dataset")
 parser.add_argument('--test_batch', type=int, default=64)
 parser.add_argument('--alpha', type=float, default=0.2)   # For beta distribution in mixup augmentation
 FLAGS = parser.parse_args()
 
-def train(model, device, train_loader, optimizer, epoch, alpha, mixup=True):
+def train(model, device, train_loader, optimizer, epoch, alpha, mixup=True, verbose=False):
     model.train()
     train_loss = 0
     for batch_idx, (data, target) in enumerate(train_loader):
@@ -45,12 +46,12 @@ def train(model, device, train_loader, optimizer, epoch, alpha, mixup=True):
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
-        if batch_idx % 100 == 0:
+        if verbose and batch_idx % 100 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
     train_loss = train_loss / len(train_loader)
-    print("avg train loss for current epoch", train_loss)
+    print(f"Avg train loss for epoch {epoch}: ", train_loss)
     return train_loss
 
 def test(model, device, test_loader):
@@ -72,7 +73,7 @@ def test(model, device, test_loader):
 
     test_loss /= len(test_loader)
     accuracy = correct/len(test_loader.dataset)
-    print(f"\n Test set: Average loss: {test_loss}, Accuracy: {accuracy}")
+    print(f"Test set average loss: {test_loss}, Accuracy: {accuracy} \n")
     return test_loss, accuracy
 
 def plot_losses(train_loss_list, test_loss_list):
@@ -95,7 +96,9 @@ def main(args):
     # Parameters
     train_kwargs = {'batch_size': args.train_batch}
     test_kwargs = {'batch_size': args.test_batch}
-    PATH = "../checkpoints/run_1"
+    PATH = "../checkpoints/Mixup/run_3"
+    if not os.path.exists(PATH):
+        os.makedirs(PATH)
 
     if use_cuda:
         cuda_kwargs = {'num_workers': 1,
@@ -135,19 +138,23 @@ def main(args):
     accuracy_list = np.array([])
     start = time.time()
     for epoch in range(1, args.epochs + 1):
-        train_loss = train(model, device, train_loader, optimizer, epoch, args.alpha)
+        train_loss = train(model, device, train_loader, optimizer, epoch, args.alpha, mixup=True)
         test_loss, accuracy = test(model, device, test_loader)
         test_loss_list = np.append(test_loss_list, test_loss)
         train_loss_list = np.append(train_loss_list, train_loss)
         accuracy_list = np.append(accuracy_list, accuracy)
         if epoch % args.checkpoint == 0:
-            torch.save(model.state_dict(), PATH + f"/model_epoch{epoch}.pt")
+            loss_list = np.concatenate((np.array([train_loss_list]), np.array([test_loss_list])), axis=0)
+            loss_list = np.concatenate((loss_list, np.array([accuracy_list])), axis=0)
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': loss_list,
+            }, PATH+f"/e{epoch}_b{args.train_batch}_lr{args.lr}.pt")
+            print(f"Checkpoint saved at epoch {epoch}")
     duration = time.time()-start
     print(f"Training done. Took {format_time(duration)}")
-
-    loss_list = np.concatenate((np.array([train_loss_list]),np.array([test_loss_list])), axis=0)
-    loss_list = np.concatenate((loss_list, np.array([accuracy_list])), axis=0)
-    np.save(PATH+f"/loss_e{args.epochs}", loss_list)
     plot_losses(train_loss_list, test_loss_list)
     print(f"Model, accuracy and loss history saved to {PATH}")
 
